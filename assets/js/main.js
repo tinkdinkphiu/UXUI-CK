b13fa9d45a5bccb08d51765403c2441c188a07e5
Le Tran Phu <52100572@student.tdtu.edu.vn>
2023-05-08 16:47:57 +0700

    add aboutus.html

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+var sendbutton = $('#footer-content-info-contactus-sendbutton');
+sendbutton.click(function() {
+    var inputField = $('#footer-content-info-contactus-inputfield');
+    $(inputField).val('');
+})
