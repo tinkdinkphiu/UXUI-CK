a4bb80a21e74aa463b3a7c30c8331048d42469fc
Le Tran Phu <52100572@student.tdtu.edu.vn>
2023-05-08 21:50:50 +0700

    login, signup, services, contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,14 @@ sendbutton.click(function() {
     var inputField = $('#footer-content-info-contactus-inputfield');
     $(inputField).val('');
 })
+
+var contact_send = $('#contact-sendbutton');
+contact_send.click(function () {
+    var contact_fullname = $('#content-input-fullname-inputfield');
+    var contact_phone = $('#content-input-phone-inputfield');
+    var contact_details = $('#content-input-details-inputfield');
+
+    $(contact_fullname).val('');
+    $(contact_phone).val('');
+    $(contact_details).val('');
+})
